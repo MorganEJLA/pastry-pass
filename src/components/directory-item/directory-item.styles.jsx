5c2f0ed439ff817e8d71c8f5358fee278137faca
MorganEJLA <mojones311@gmail.com>
2025-09-07 16:43:44 -0400

    fixed responive issue

--- src/components/directory-item/directory-item.styles.jsx
+++ src/components/directory-item/directory-item.styles.jsx
@@ -9,60 +9,69 @@ export const BackgroundImage = styled.div`
 `;
 
 export const Body = styled.div`
-  height: 90px;
-  padding: 0 25px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  width: 80%;
+  padding: 0.5rem;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
   border: 1px solid black;
   background-color: white;
   opacity: 0.7;
-  position: absolute;
+  text-align: center;
 
   h2 {
     font-weight: bold;
-    margin: 0 6px 0;
-    font-size: 22px;
+    margin: 0;
+    font-size: clamp(16px, 2vw, 22px);
     color: #4a4a4a;
     text-transform: uppercase;
   }
 
   p {
     font-weight: lighter;
-    font-size: 16px;
+    font-size: clamp(12px, 1.5vw, 16px);
   }
 `;
 
 export const DirectoryItemContainer = styled.div`
   min-width: 30%;
-  height: 240px;
   flex: 1 1 auto;
   display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
   align-items: center;
-  justify-content: center;
   border: 1px solid black;
   margin: 0 7.5px 15px;
   overflow: hidden;
-
-  &:first-child {
-    margin-right: 7.5px;
-  }
-
-  &:last-child {
-    margin-left: 7.5px;
-  }
+  position: relative;
+  height: auto;
+  aspect-ratio: 3 / 2; /* Keeps consistent tile shape, optional */
 
   &:hover {
     cursor: pointer;
 
     ${BackgroundImage} {
       transform: scale(1.1);
-      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
     }
 
     ${Body} {
       opacity: 0.9;
     }
+  }
+
+  @media (max-width: 992px) {
+    min-width: 45%;
+  }
+
+  @media (max-width: 768px) {
+    min-width: 100%;
+    margin: 0 0 15px 0;
   }
 `;
